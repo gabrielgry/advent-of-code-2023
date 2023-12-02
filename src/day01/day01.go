ce22b4e93fcd9a0724b6a6170da94e6505cf4761
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2023-12-02 16:24:55 -0300

    Add solution day01 part 2

--- src/day01/day01.go
+++ src/day01/day01.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 	"unicode/utf8"
 )
@@ -34,33 +36,101 @@ func readLinesFromFile(filename string) ([]string, error) {
 	return lines, nil
 }
 
-func findFirstDigit(code string) rune {
-	for _, char := range code {
+func mergeDigitMaps(a map[int]rune, b map[int]rune) map[int]rune {
+	merged := make(map[int]rune)
+
+	for key, value := range a {
+		merged[key] = value
+	}
+
+	for key, value := range b {
+		merged[key] = value
+	}
+
+	return merged
+}
+
+func findPlainDigits(line string) map[int]rune {
+	digits := make(map[int]rune)
+
+	for i, char := range line {
 		if unicode.IsDigit(char) {
-			return char
+			digits[i] = char
 		}
 	}
 
-	return utf8.RuneError
+	return digits
 }
 
-func findLastDigit(code string) rune {
-	for i, w := len(code), 0; i >= 0; i = i - w {
-		char, size := utf8.DecodeLastRuneInString(code[:i])
+func findSpelledDigits(line string) map[int]rune {
+	digits := make(map[int]rune)
 
-		if unicode.IsDigit(char) {
-			return char
+	digits[strings.Index(line, "one")] = '1'
+	digits[strings.Index(line, "two")] = '2'
+	digits[strings.Index(line, "three")] = '3'
+	digits[strings.Index(line, "four")] = '4'
+	digits[strings.Index(line, "five")] = '5'
+	digits[strings.Index(line, "six")] = '6'
+	digits[strings.Index(line, "seven")] = '7'
+	digits[strings.Index(line, "eight")] = '8'
+	digits[strings.Index(line, "nine")] = '9'
+
+	digits[strings.LastIndex(line, "one")] = '1'
+	digits[strings.LastIndex(line, "two")] = '2'
+	digits[strings.LastIndex(line, "three")] = '3'
+	digits[strings.LastIndex(line, "four")] = '4'
+	digits[strings.LastIndex(line, "five")] = '5'
+	digits[strings.LastIndex(line, "six")] = '6'
+	digits[strings.LastIndex(line, "seven")] = '7'
+	digits[strings.LastIndex(line, "eight")] = '8'
+	digits[strings.LastIndex(line, "nine")] = '9'
+
+	delete(digits, -1)
+
+	return digits
+}
+
+func findAllDigits(line string) map[int]rune {
+	plainDigits := findPlainDigits(line)
+	spelledDigits := findSpelledDigits(line)
+
+	merged := mergeDigitMaps(plainDigits, spelledDigits)
+
+	return merged
+}
+
+func getFirstDigit(digits map[int]rune) rune {
+	smallestIndex := math.MaxInt
+	firstDigit := utf8.RuneError
+
+	for index := range digits {
+		if index < smallestIndex {
+			smallestIndex = index
+			firstDigit = digits[index]
 		}
-
-		w = size
 	}
 
-	return utf8.RuneError
+	return firstDigit
 }
 
-func findNumberCode(word string) (int, error) {
-	firstDigit := findFirstDigit(word)
-	lastDigit := findLastDigit(word)
+func getLastDigit(digits map[int]rune) rune {
+	biggestIndex := -1
+	lastDigit := utf8.RuneError
+
+	for index := range digits {
+		if index > biggestIndex {
+			biggestIndex = index
+			lastDigit = digits[index]
+		}
+	}
+
+	return lastDigit
+}
+
+func findNumberCode(line string) (int, error) {
+	digits := findAllDigits(line)
+	firstDigit := getFirstDigit(digits)
+	lastDigit := getLastDigit(digits)
 
 	var fullNumber string
 
@@ -92,7 +162,7 @@ func getSumOfCodes(lines []string) int {
 }
 
 func main() {
-	lines, err := readLinesFromFile("../../input/day01")
+	lines, err := readLinesFromFile("../../inputs/day01/input")
 
 	if err != nil {
 		log.Fatal("Could not open the input file")
